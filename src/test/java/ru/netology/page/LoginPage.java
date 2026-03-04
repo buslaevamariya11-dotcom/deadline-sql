dbadf7646581325329dddab4576d431e4521c6f1
Mariya Buslaeva <buslaeva.mariya11@gmail.com>
2026-03-04 10:42:16 +0400

    Test confirms user is not blocked after 3 invalid login attempts

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -13,17 +13,19 @@ public class LoginPage {
     private SelenideElement loginButton = $("[data-test-id=action-login]");
     private SelenideElement errorNotification = $("[data-test-id=error-notification]");
 
-    public VerificationPage validLogin(DataHelper.AuthInfo authInfo) {
+    private void login(DataHelper.AuthInfo authInfo) {
         loginField.setValue(authInfo.getLogin());
         passwordField.setValue(authInfo.getPassword());
         loginButton.click();
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo authInfo) {
+        login(authInfo);
         return new VerificationPage();
     }
 
     public void invalidLogin(DataHelper.AuthInfo authInfo) {
-        loginField.setValue(authInfo.getLogin());
-        passwordField.setValue(authInfo.getPassword());
-        loginButton.click();
+        login(authInfo);
         errorNotification
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Ошибка"));
